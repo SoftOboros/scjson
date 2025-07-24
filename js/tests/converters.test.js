5c4adaff109a89b37cdaf2d3e2c773604540423c
Ira Abbott <ira@mycloud.blue>
2025-07-24 13:13:17 -0400

    Fix token splitting and ordering

--- js/tests/converters.test.js
+++ js/tests/converters.test.js
@@ -19,3 +19,15 @@ test('script text becomes object with content', () => {
   expect(Array.isArray(obj.script)).toBe(true);
   expect(obj.script[0].content[0]).toBe('foo');
 });
+
+/**
+ * Ensure whitespace token attributes are split into arrays.
+ */
+test('transition target tokens split correctly', () => {
+  const xml = '<scxml xmlns="http://www.w3.org/2005/07/scxml"><state id="s1"><transition target="a b"/></state></scxml>';
+  const jsonStr = xmlToJson(xml);
+  const obj = JSON.parse(jsonStr);
+  const trans = obj.state[0].transition[0];
+  expect(Array.isArray(trans.target)).toBe(true);
+  expect(trans.target).toEqual(['a', 'b']);
+});
